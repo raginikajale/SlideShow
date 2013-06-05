ec987b8018395d3203e630afe0db75c5eb67c36b
raginikajale <raginikajale@gmail.com>
2013-06-05 17:17:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-  
+  var arr=jQuery.makeArray(document.getElementsByTagName('img'));
+    var current=0;
   setInterval(function () {
-    	$('img').current('img').fadeOut(1000)
-			.next('img').fadeIn(1000);
-	}, 3000);
-
+    $(arr[current]).fadeOut(1000,function(){
+             
+             $(arr[current+1]).fadeIn(2000);
+             $(arr[current+1]).fadeOut(2000);
+             if(current==arr.length)
+             {
+             	current=0;
+             }
+             } )}   
+          , 4000);
      
 
 });
