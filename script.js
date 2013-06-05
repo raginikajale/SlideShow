b503d2a9d7419052bb2e51b948cc08f5e7f6fd28
raginikajale <raginikajale@gmail.com>
2013-06-05 13:40:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 $(document).ready(function(){
   
   setInterval(function () {
     	$('#slideshow :first-child').fadeOut(1000)
-			.next('img').fadeIn(1000).end();
+			.next('img').fadeIn(1000);
 	}, 3000);
 
      
